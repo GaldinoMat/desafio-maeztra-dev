8a1c757d6db4c887bbca85fe4568fe16acc8682a
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-04-25 13:04:25 -0300

    feat: finish question 2

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,1 +1,18 @@
+const checkDuplicates = (arr) => {
+  /**
+    This function filters out any element whose index matches the elementIndex argument, which means that if they match, 
+    they are an unique value
+  */
+  const duplicates = arr.filter(
+    (item, elementIndex) => arr.indexOf(item) !== elementIndex
+  );
 
+  /**
+    Returns a Set, a data structure in javascript that contains unique values, avoiding multiple values in the "duplicates" 
+    variables and only focusing in the values that are duplicated. We use the [] notation to turn it into an array and the 
+    spread operator to get the values from the object it returns
+  */
+  return [...new Set(duplicates)];
+};
+
+console.log(checkDuplicates([4, 5, 44, 98, 4, 5, 6, 7]));
